b4137b55728e97fe2f78fda0a97e758576749684
Conzel <38732545+Conzel@users.noreply.github.com>
2022-02-16 14:30:26 +0100

    ADD: Tests for the compression with constriction

--- tests/test_constriction_compression.py
+++ tests/test_constriction_compression.py
@@ -1,6 +1,13 @@
-from compression import compress_symbols, decompress_symbols, make_symbols, quantize, cdf_to_pmf, unmake_symbols
+import os
+from posixpath import dirname
+from compression import compress_symbols, decompress_symbols, encompression_decompression_run, make_symbols, quantize, cdf_to_pmf, unmake_symbols, _mock_quantization
 import numpy as np
 from compressai._CXX import pmf_to_quantized_cdf
+import torch
+from export_weights import clean_checkpoint_data_parallel
+from models import FactorizedPrior
+from PIL import Image
+from utils import pil_to_tensor
 
 
 def test_quantization():
@@ -74,4 +81,69 @@ def test_make_unmake_symbols_random():
     # we can maximally introduce a 0.5 error due to quantization
     assert (np.abs(y_restored - y) <= 0.5).all()
     # sanity check, would be unrealistic to only introduce a 0.1 error
-    assert (np.abs(y_restored -y) < 0.1).any()
+    assert (np.abs(y_restored - y) < 0.1).any()
+
+
+def test_symbols_retained_during_compression():
+    """
+    Tests if offset and means have been correctly added and removed again.
+
+    This is the normal process:
+    y -> make_symbols -> compress -> decompress -> unmake symbols -> y_hat
+
+    The mock quantization function provides this:
+    y --------------------- mock quantization ---------------------> y_hat 
+
+    These two should be identical
+    """
+    np.random.seed(5)
+    offsets = np.array([-6, -7, -7])
+    symbols_per_channel = np.array([13, 14, 15])
+    y = np.random.random((3, 12, 15))
+    means = np.random.random(offsets.shape)
+
+    symbols = make_symbols(y, offsets, symbols_per_channel, means)
+    y_restored = unmake_symbols(symbols, offsets, means)
+
+    y_quant_mock = _mock_quantization(y, offsets, symbols_per_channel, means)
+    assert np.allclose(y_restored, y_quant_mock)
+
+
+def test_performance_against_compressai_implementation():
+    checkpoint = torch.load(os.path.join(dirname(__file__), "assets/checkpoint.pth.tar"),
+                            map_location=torch.device("cpu"))
+
+    new_dict = clean_checkpoint_data_parallel(checkpoint)
+
+    # Restoring the model
+    model = FactorizedPrior(128, 192)
+    model.load_state_dict(new_dict)
+    model.update()
+
+    with Image.open(os.path.join(dirname(__file__), "assets/test-img-link-small.jpg")) as im:
+        x = pil_to_tensor(im.crop((0, 0, 256, 256)))
+        # the implementation by CompressAI
+        s = model.compress(x)
+        bytes_cpai = len(s['strings'][0][0])
+        x_hat = model.decompress(s["strings"], s["shape"])["x_hat"]
+        print(x.shape, x_hat.shape)
+        mse_cpai = (x - x_hat).pow(2).mean().item()
+
+        # our implementation
+        medians = model.entropy_bottleneck.quantiles[:, 0, 1].detach().numpy()
+        y = model.analysis_transform(x)
+        compressed, y_quant = encompression_decompression_run(y.squeeze().detach().numpy(), model.entropy_bottleneck._quantized_cdf.numpy(
+        ), model.entropy_bottleneck._offset.numpy(), model.entropy_bottleneck._cdf_length.numpy(), 16, means=medians)
+
+        x_hat_constriction = model.synthesis_transform(
+            torch.Tensor(y_quant[None, :, :, :])).clamp_(0, 1)
+        bytes_constriction = compressed.size*4  # *32/8, as compress is uint32-array
+        mse_constriction = (x - x_hat_constriction).pow(2).mean().item()
+
+    print(f"MSE reached: {mse_constriction}")
+    print(f"Bytes used: {bytes_constriction}")
+    assert mse_cpai == mse_constriction
+    assert np.abs(bytes_cpai - bytes_constriction) < 10
+    # Mini-regression test
+    assert mse_constriction == 0.0018695825710892677
+    assert bytes_constriction == 5144
