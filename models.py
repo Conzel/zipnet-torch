e83f3f93a8750b188fe17b04fdcd715fc8280740
Conzel <38732545+Conzel@users.noreply.github.com>
2022-03-03 09:33:35 +0100

    FIX: Removed merge notice from models

--- models.py
+++ models.py
@@ -27,11 +27,8 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-<<<<<<< HEAD
 from collections import OrderedDict
-=======
 from typing import Callable, Optional, OrderedDict
->>>>>>> main
 import warnings
 import numpy as np
 import torch
